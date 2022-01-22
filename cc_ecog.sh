3f02675a5b9184b4c99b636624ed7ee08071376d
nikhil.garg <nikhil.garg@usherbrooke.ca>
2022-01-22 06:41:24 +0100

    main

--- cc_ecog.sh
+++ cc_ecog.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --cpus-per-task=4
-#SBATCH --time=0:30:00 # DD-HH:MM:SS
-#SBATCH --mem-per-cpu=2GB
-#SBATCH --array=0-5000
+#SBATCH --cpus-per-task=2
+#SBATCH --time=0:5:00 # DD-HH:MM:SS
+#SBATCH --mem-per-cpu=4GB
+#SBATCH --array=0-999
 #SBATCH --job-name=ecog_encoding
 
 echo "Moving files"
@@ -11,8 +11,7 @@ cd $SLURM_TMPDIR/spike_encoding_toolbox
 
 echo "Starting application"
 mkdir -p "$HOME/ecog_results/"
-
-if $HOME/env/bin/python evaluate_encoder.py run_random_experiment $SLURM_ARRAY_TASK_ID; then
+if $HOME/env/bin/python run_random_experiment.py --seed $SLURM_ARRAY_TASK_ID; then
     echo "Copying results"
     mv "ecog_$SLURM_ARRAY_TASK_ID.txt" "$HOME/ecog_results/"
 fi
--- run_random_experiment.py
+++ run_random_experiment.py
@@ -35,40 +35,45 @@ from sklearn import metrics as metrics
 # import autosklearn.classification
 from evaluate_encoder import  *
 
-def run_random_experiment(seed):
+def run_random_experiment(args):
     args = my_args()
+    seed = int(args.seed)
     
     np.random.seed(seed)
-    seed=np.random.randint(0, 0xFFFFFFF),
-    args.encode_thr_up=np.random.uniform(0.1, 2)
-    args.encode_thr_dn=np.random.uniform(0.1, 2)
+    # seed=np.random.randint(0, 0xFFFFFFF),
+    args.encode_thr_up=np.random.uniform(0.5, 2)
+    args.encode_thr_dn=np.random.uniform(0.5, 2)
     args.tstep=np.random.choice([200,300,500,600,1000,1500,3000])
-    args.interpfact=np.random.choice([1,2,3,4])
+    args.interpfact=np.random.choice([1,2,3])
     args.refractory=np.random.choice([0,1,2]) 
     input_args=[args.encode_thr_up,args.encode_thr_dn,args.tstep,args.interpfact,args.refractory]
     input_args_string=map(str,input_args)
     output_file = f"ecog_{seed}.txt"
-    if os.path.exists(output_file):
-        exit(0)
+    # # if os.path.exists(output_file):
+    # #     exit(0)
 
     svm_score_input,rf_score_input,avg_spike_rate, svm_score_baseline, svm_score_comb, rf_score_comb = evaluate_encoder(args)
+    results=[svm_score_input,rf_score_input,avg_spike_rate, svm_score_baseline, svm_score_comb, rf_score_comb]
+    
     with open(output_file, "w") as f:
-        results=[svm_score_input,rf_score_input,avg_spike_rate, svm_score_baseline, svm_score_comb, rf_score_comb]
-        f.write(str(input_args_string))
+        f.write(str(input_args))
         f.write(str(results))
+        # f.write('haha')
+        
 
 if __name__ == '__main__':
     logger = logging.getLogger(__name__)
 
     args = my_args()
+    seed=int(args.seed)
     print(args.__dict__)
     logging.basicConfig(level=logging.DEBUG)
     # Fix the seed of all random number generator
-    seed = 500
+    # seed = 500
     random.seed(seed)
     np.random.seed(seed)
-    svm_score_input,rf_score_input,avg_spike_rate, svm_score_baseline, svm_score_comb, rf_score_comb= evaluate_encoder(args)
-    print('Average spike rate :')
-    print(avg_spike_rate)
-    print('Accuraccy input: ' + str(svm_score_input))
+    run_random_experiment(args)
+    # print('Average spike rate :')
+    # print(avg_spike_rate)
+    # print('Accuraccy input: ' + str(svm_score_input))
     logger.info('All done.')

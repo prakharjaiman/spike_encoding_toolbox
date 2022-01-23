fb9a01d049a0766625a7a50f0bfc884af2c0fdf1
nikhil.garg <nikhil.garg@usherbrooke.ca>
2022-01-23 23:47:02 +0100

    up

--- run_random_experiment.py
+++ run_random_experiment.py
@@ -38,38 +38,70 @@ from evaluate_encoder import  *
 def run_random_experiment(args):
     args = my_args()
     seed = int(args.seed)
-    
     np.random.seed(seed)
-    # seed=np.random.randint(0, 0xFFFFFFF),
-    args.encode_thr_up=np.random.uniform(0.1, 2)
-    args.encode_thr_dn=np.random.uniform(0.1, 2)
-    args.tstep=np.random.choice([200,300,500,600,1000,1500,3000])
-    args.interpfact=np.random.choice([1])
-    args.refractory=np.random.choice([1]) 
-    input_args=[args.encode_thr_up,args.encode_thr_dn,args.tstep,args.interpfact,args.refractory]
-    input_args_string=map(str,input_args)
-    output_file = f"ecog_{seed}.csv"
-    # # if os.path.exists(output_file):
-    # #     exit(0)
-    
-    svm_score, rf_score, firing_rate, svm_score_baseline, svm_score_comb, rf_score_comb = evaluate_encoder(args)
-    # results=[svm_score_input,rf_score_input,avg_spike_rate, svm_score_baseline, svm_score_comb, rf_score_comb]
+    datasets = [ "ca_mot", "de_mot", "jt_mot", "gf_mot"]
     df = pd.DataFrame({	"dataset":[],"encode_thr_up":[],"encode_thr_dn":[],"tstep":[],"encode_refractory":[],"encode_interpfact":[],"firing_rate":[],"svm_score":[],"rf_score":[],"svm_score_baseline":[],"svm_score_comb":[],"rf_score_comb":[], "auto_score":[]})
-    df = df.append({ "dataset":args.dataset,
-						 "fold":args.fold,
-						 "encode_thr_up":args.encode_thr_up,
-						 "encode_thr_dn":args.encode_thr_dn,
-						 "tstep": args.tstep,
-						 "encode_refractory": args.encode_refractory,
-						 "encode_interpfact": args.encode_interpfact,			 
-		                 "firing_rate":firing_rate,
-		                 "svm_score":svm_score,
-                     	 "rf_score":rf_score,
-		                 "svm_score_baseline":svm_score_baseline,
-						 "svm_score_comb":svm_score_comb,
-						 "rf_score_comb":rf_score_comb
-						#  "auto_score":auto_score
-		                 },ignore_index=True)
+
+    if len(datasets)>1:
+        for i in range(len(datasets)):
+            args.dataset = datasets[i]
+            args.encode_thr_up=np.random.uniform(0.1, 2)
+            args.encode_thr_dn=np.random.uniform(0.1, 2)
+            args.tstep=np.random.choice([200,300,500,600,1000,1500,3000])
+            args.interpfact=np.random.choice([1])
+            args.refractory=np.random.choice([1]) 
+            input_args=[args.dataset,args.encode_thr_up,args.encode_thr_dn,args.tstep,args.interpfact,args.refractory]
+            svm_score, rf_score, firing_rate, svm_score_baseline, svm_score_comb, rf_score_comb = evaluate_encoder(args)
+            df = df.append({ "dataset":args.dataset,
+                                "fold":args.fold,
+                                "encode_thr_up":args.encode_thr_up,
+                                "encode_thr_dn":args.encode_thr_dn,
+                                "tstep": args.tstep,
+                                "encode_refractory": args.encode_refractory,
+                                "encode_interpfact": args.encode_interpfact,			 
+                                "firing_rate":firing_rate,
+                                "svm_score":svm_score,
+                                "rf_score":rf_score,
+                                "svm_score_baseline":svm_score_baseline,
+                                "svm_score_comb":svm_score_comb,
+                                "rf_score_comb":rf_score_comb
+                                #  "auto_score":auto_score
+                                },ignore_index=True)
+        output_file = f"ecog_{seed}.csv"
+        df.to_csv(output_file, index=False)
+
+    else: 
+    # seed=np.random.randint(0, 0xFFFFFFF),
+        args.encode_thr_up=np.random.uniform(0.1, 2)
+        args.encode_thr_dn=np.random.uniform(0.1, 2)
+        args.tstep=np.random.choice([200,300,500,600,1000,1500,3000])
+        args.dataset = ["zt_mot", "jc_mot", "fp_mot", "jp_mot", "jm_mot", "ca_mot", "gc_mot", "ug_mot", "wc_mot", "rr_mot", "rh_mot", "gf_mot", "bp_mot", "jt_mot","jf_mot", "cc_mot", "de_mot", "hh_mot", "hl_mot"]
+        args.interpfact=np.random.choice([1])
+        args.refractory=np.random.choice([1]) 
+        input_args=[args.dataset,args.encode_thr_up,args.encode_thr_dn,args.tstep,args.interpfact,args.refractory]
+        input_args_string=map(str,input_args)
+        output_file = f"ecog_{seed}.csv"
+        # # if os.path.exists(output_file):
+        # #     exit(0)
+        
+        svm_score, rf_score, firing_rate, svm_score_baseline, svm_score_comb, rf_score_comb = evaluate_encoder(args)
+        # results=[svm_score_input,rf_score_input,avg_spike_rate, svm_score_baseline, svm_score_comb, rf_score_comb]
+        df = pd.DataFrame({	"dataset":[],"encode_thr_up":[],"encode_thr_dn":[],"tstep":[],"encode_refractory":[],"encode_interpfact":[],"firing_rate":[],"svm_score":[],"rf_score":[],"svm_score_baseline":[],"svm_score_comb":[],"rf_score_comb":[], "auto_score":[]})
+        df = df.append({ "dataset":args.dataset,
+                            "fold":args.fold,
+                            "encode_thr_up":args.encode_thr_up,
+                            "encode_thr_dn":args.encode_thr_dn,
+                            "tstep": args.tstep,
+                            "encode_refractory": args.encode_refractory,
+                            "encode_interpfact": args.encode_interpfact,			 
+                            "firing_rate":firing_rate,
+                            "svm_score":svm_score,
+                            "rf_score":rf_score,
+                            "svm_score_baseline":svm_score_baseline,
+                            "svm_score_comb":svm_score_comb,
+                            "rf_score_comb":rf_score_comb
+                            #  "auto_score":auto_score
+                            },ignore_index=True)
     df.to_csv(output_file, index=False)
         # f.write('haha')
         

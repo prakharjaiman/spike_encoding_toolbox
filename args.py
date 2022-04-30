8453a5b4752cfa439554132ef4021609e3f874a2
Ritwik Katariya <58631634+ritkat@users.noreply.github.com>
2022-05-01 00:17:49 +0530

    Update args.py

--- args.py
+++ args.py
@@ -102,6 +102,10 @@ def args():
     parser.add_argument('--fold', default=3, type=float,
                         help='Fold for train/test'
                              'e.g. 1, 2, 3 ')
+    
+    parser.add_argument('--population', default=300, type=int,
+                        help='population size for genetic search'
+                             'e.g. 100, 200, 300 ')
 
     parser.add_argument('--gen', default=2, type=int,
                     help='Fold for train/test'

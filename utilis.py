48868ad63dc80120304dec0f2a27b925c3dc0ba3
Ritwik Katariya <58631634+ritkat@users.noreply.github.com>
2022-05-03 00:04:07 +0530

    Update utilis.py

--- utilis.py
+++ utilis.py
@@ -377,13 +377,11 @@ def bandpassfun (start, stop, nbbanks):
         i+=1
     return bank_array
 
-def butter_bandpass(lowcut, highcut, fs, order=5):
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=7):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
     b, a = butter(order, [lowcut, highcut],fs=fs, btype='band')
-    return b, a
-
-
-def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
-    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
     y = lfilter(b, a, data)
     return y
 
